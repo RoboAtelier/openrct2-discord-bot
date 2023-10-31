9c9f98b37c6772af9649a471ae0794de46d0ac80
RoboAtelier <roboatelier@gmail.com>
2023-10-30 22:13:48 -0400

    feat: Add new classes for reworked BotCommand base

---
+++ src/modules/discord/commands/command-access-result.ts
@@ -0,0 +1,54 @@
+class CommandAccessResult {
+  constructor(
+    readonly canUseCommand: boolean,
+    readonly deniedSubcommandGroup?: string,
+    readonly deniedSubcommand?: string,
+    readonly deniedOptions?: string[]
+  ) { };
+
+  get isSuccess() {
+    return this.canUseCommand
+      && !this.deniedSubcommandGroup
+      && !this.deniedSubcommand
+      && !(this.deniedOptions && this.deniedOptions.length)
+  };
+};
+
+export class CommandAccessResultBuilder {
+  private canUseCommand = false;
+  private deniedSubcommandGroup?: string;
+  private deniedSubcommand?: string;
+  private deniedOptions?: string[];
+
+  withAccess(access: boolean) {
+    this.canUseCommand = access;
+    return this;
+  };
+
+  withDeniedSubcommandGroup(groupName: string) {
+    this.deniedSubcommandGroup = groupName;
+    return this;
+  };
+
+  withDeniedSubcommand(subcommandName: string) {
+    this.deniedSubcommand = subcommandName;
+    return this;
+  };
+
+  withDeniedOptions(...optionNames: string[]) {
+    if (!this.deniedOptions) {
+      this.deniedOptions = [];
+    };
+    this.deniedOptions.push(...optionNames);
+    return this;
+  };
+
+  resolve() {
+    return new CommandAccessResult(
+      this.canUseCommand,
+      this.deniedSubcommandGroup,
+      this.deniedSubcommand,
+      this.deniedOptions
+    );
+  };
+};
